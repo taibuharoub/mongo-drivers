51fe23b6a73c2b779d310d6ae6fd5d3e7b65d540
Taibu Haroub <haroubtaibu@gmail.com>
2021-08-19 21:14:29 +0300

    Set up auth routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
 import productRoutes from "./products.js";
+import authRoutes from "./auth.js";
 
 export default (app) => {
-  // app.get("/", (req, res, next) => {
-  //   res.status(200).json({ message: "From Mongo shell to Drivers" });
-  // });
+  app.get("/", (req, res, next) => {
+    res.status(200).json({ message: "From Mongo shell to Drivers" });
+  });
   app.use("/products", productRoutes);
+  app.use("/", authRoutes);
 };
